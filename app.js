be04ca1dbfd8aca04ad99999390ef9b103176956
Aaron L. Zeng <a2z@mit.edu>
2015-09-18 02:11:58 -0400

    split links into separate module

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
 (function() {
-    var app = angular.module('website', []);
-
-    app.directive('links', function() {
-	return {
-	    restrict: 'E',
-	    templateUrl: 'links.html',
-	    controller: function() {
-		this.links = [
-		    {
-			url: 'http://linkedin.com/in/zenga/',
-			label: 'LinkedIn Profile',
-		    },
-		    {
-			url: '/resume/aaron-zeng-resume.pdf',
-			label: 'Resume (PDF)',
-		    },
-		];
-	    },
-	    controllerAs: 'linksCtrl',
-	};
-    });
+    var app = angular.module('website', [
+	'links',
+    ]);
 })();
---
+++ links.js
@@ -0,0 +1,23 @@
+(function() {
+    var app = angular.module('links', []);
+
+    app.directive('links', function() {
+	return {
+	    restrict: 'E',
+	    templateUrl: 'links.html',
+	    controller: function() {
+		this.links = [
+		    {
+			url: 'http://linkedin.com/in/zenga/',
+			label: 'LinkedIn Profile',
+		    },
+		    {
+			url: '/resume/aaron-zeng-resume.pdf',
+			label: 'Resume (PDF)',
+		    },
+		];
+	    },
+	    controllerAs: 'linksCtrl',
+	};
+    });
+})();
