950773ce65c07184e634b59ce696e406e08f24e4
Aaron L. Zeng <a2z@mit.edu>
2015-10-01 18:43:43 -0400

    add link to personal website (b4.dmz.se)

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 (function() {
+    var personalSite = 'http://b4.dmz.se';
+
+    $(document).ready(function() {
+        $('#personal-site').find('a').attr('href', personalSite);
+        $('#personal-site')
+            .find('script.updownquery')
+            .attr('src', personalSite + '/api/updown?jsonp=updateLinkUpDown');
+        updateLinkUpDown();
+    });
+
+    window.updateLinkUpDown = function(obj) {
+        if (obj.up) {
+            $('#personal-site').find('.updown').html('up')
+                .removeClass('linkdown')
+                .addClass('linkup');
+        } else {
+            $('#personal-site').find('.updown').html('down')
+                .removeClass('linkup')
+                .addClass('linkdown');
+        }
+    }
+
     var app = angular.module('website', [
 	'links',
     ]);
