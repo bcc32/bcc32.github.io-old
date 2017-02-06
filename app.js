8258b261ba27a2e4b2aedbffb954e5c4706d2bc5
Aaron L. Zeng <a2z@mit.edu>
2017-02-05 20:14:46 -0500

    Only display down after request fails

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
     var personalSite = 'http://bcc32.com';
 
     function updateLinkUpDown() {
-        $('#updown').html('down').removeClass('linkup').addClass('linkdown');
+        $('#updown').html('...').removeClass('linkup linkdown');
 
         var nonce = Math.floor(32768 * Math.random());
         $.ajax({
@@ -14,7 +14,12 @@
         .done(function (data) {
             if (data.nonce === ~nonce) {
                 $('#updown').html('up').removeClass('linkdown').addClass('linkup');
+            } else {
+                throw new Error('incorrect nonce');
             }
+        })
+        .fail(function () {
+            $('#updown').html('down').removeClass('linkup').addClass('linkdown');
         });
     }
 
