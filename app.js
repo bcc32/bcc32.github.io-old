dae56378309eddad1ffc1e40e37b662e671efd46
Aaron L. Zeng <a2z@mit.edu>
2017-02-05 20:12:44 -0500

    Use jQuery for JSONP

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 (function() {
-    var personalSite = 'http://b4.zsh.jp';
+    var personalSite = 'http://bcc32.com';
+
+    function updateLinkUpDown() {
+        $('#updown').html('down').removeClass('linkup').addClass('linkdown');
+
+        var nonce = Math.floor(32768 * Math.random());
+        $.ajax({
+            dataType: 'jsonp',
+            data: 'nonce=' + nonce,
+            jsonp: 'pong',
+            url: personalSite + '/api/ping',
+        })
+        .done(function (data) {
+            if (data.nonce === ~nonce) {
+                $('#updown').html('up').removeClass('linkdown').addClass('linkup');
+            }
+        });
+    }
 
     $(document).ready(function() {
-        $('#personal-site')
-          .find('a')
-          .attr('href', personalSite);
-        $('#personal-site')
-            .find('script.updownquery')
-            .attr('src', personalSite + '/api/updown?jsonp=updateLinkUpDown');
+        $('#personal-site-link').attr('href', personalSite);
         updateLinkUpDown();
     });
-
-    window.updateLinkUpDown = function(obj) {
-        if (obj.up) {
-            $('#personal-site').find('.updown').html('up')
-                .removeClass('linkdown')
-                .addClass('linkup');
-        } else {
-            $('#personal-site').find('.updown').html('down')
-                .removeClass('linkup')
-                .addClass('linkdown');
-        }
-    }
 
     var app = angular.module('website', [
 	'links',
